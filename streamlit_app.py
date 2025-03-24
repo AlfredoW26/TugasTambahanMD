e5443d809959403dd1affb468f0ee4dafc6c7e53
AlfredoW26 <147214013+AlfredoW26@users.noreply.github.com>
2025-03-24 19:56:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,12 @@ loaded_encoder = joblib.load('encoding.pkl')
 loaded_scaler = joblib.load('scaling.pkl')
 
 def input_to_df(input):
-    data = [input]
-    df = pd.DataFrame(data, columns=[
+    df = pd.DataFrame([input], columns=[
         'Gender', 'Age', 'Height', 'Weight', 'family_history_with_overweight', 
         'FAVC', 'FCVC', 'NCP', 'CAEC', 'SMOKE', 'CH2O', 'SCC', 'FAF', 'TUE', 'CALC', 'MTRANS'
     ])
     return df
-    
+
 def encode(df):
     for column in df.columns:
         if column in loaded_encoder:
@@ -28,60 +27,60 @@ def normalize(df):
     return pd.DataFrame(df, columns=loaded_scaler.feature_names_in_)
 
 def predict_with_model(model, user_input):
-    prediction = model.predict(user_input)
+    user_input = user_input[model.feature_names_in_]  
+    prediction = model.predict(user_input.to_numpy())
     return prediction[0]
 
 def main():
-  st.title('Machine Learning App')
-  st.info('This app will predict your obesity level!')
+    st.title('Machine Learning App')
+    st.info('This app will predict your obesity level!')
 
-  with st.expander('**Data**'):
-    df = pd.read_csv('ObesityDataSet_raw_and_data_sinthetic.csv')
-    st.write(df)
-    
-  with st.expander('**Data Visualization**'):
-    st.scatter_chart(data=df, x='Height', y='Weight', color='NObeyesdad')
-     
-  # Input data oleh user
-  gender = st.selectbox('Gender', ('Male', 'Female'))
-  age = st.slider('Age', min_value=0, max_value=75, value=21)
-  height = st.slider('Height', min_value=0.50, max_value=2.00, value=1.62)  
-  weight = st.slider('Weight', min_value=10.0, max_value=140.0, value=64.0) 
-  family_history_with_overweight = st.selectbox('Family History With Overweight', ('yes', 'no'))
-  FAVC = st.selectbox('Frequent Consumption of High Caloric Food', ('yes', 'no')) 
-  FCVC = st.slider('Vegetable Consumption Frequency', min_value=0.0, max_value=3.0, value=2.0)
-  NCP = st.slider('Number of Meals per Day', min_value=0.0, max_value=3.0, value=2.0)
-  CAEC = st.selectbox('Consumption of Food Between Meals', ('no', 'Sometimes', 'Frequently', 'Always')) 
-  SMOKE = st.selectbox('Smoking Habit', ('yes', 'no'))
-  CH2O = st.slider('Daily Water Intake', min_value=0.0, max_value=3.0, value=2.0)
-  SCC = st.selectbox('Caloric Drinks Consumption', ('yes', 'no'))
-  FAF = st.slider('Physical Activity Frequency', min_value=0.0, max_value=3.0, value=2.0)
-  TUE = st.slider('Time Using Technology', min_value=0.0, max_value=3.0, value=2.0)
-  CALC = st.selectbox('Alcohol Consumption', ('no', 'Sometimes', 'Frequently', 'Always')) 
-  MTRANS = st.selectbox('Transportation Mode', ('Automobile', 'Bike', 'Motorbike', 'Public_Transportation', 'Walking'))
+    with st.expander('**Data**'):
+        df = pd.read_csv('ObesityDataSet_raw_and_data_sinthetic.csv')
+        st.write(df)
 
-  # Mengumpulkan input data dalam list
-  user_input = [gender, age, height, weight, family_history_with_overweight, 
-                FAVC, FCVC, NCP, CAEC, SMOKE, CH2O, SCC, FAF, TUE, CALC, MTRANS]
-    
-  df_input = input_to_df(user_input)
-    
-  st.write('Data input by user')
-  st.write(df_input)
+    with st.expander('**Data Visualization**'):
+        st.scatter_chart(data=df, x='Height', y='Weight', color='NObeyesdad')
 
-  df_input = encode(df_input)
-  df_input = normalize(df_input)
-  prediction = predict_with_model(model, df_input)
+    gender = st.selectbox('Gender', ('Male', 'Female'))
+    age = st.slider('Age', min_value=0, max_value=75, value=21)
+    height = st.slider('Height', min_value=0.50, max_value=2.00, value=1.62)  
+    weight = st.slider('Weight', min_value=10.0, max_value=140.0, value=64.0) 
+    family_history_with_overweight = st.selectbox('Family History With Overweight', ('yes', 'no'))
+    FAVC = st.selectbox('Frequent Consumption of High Caloric Food', ('yes', 'no')) 
+    FCVC = st.slider('Vegetable Consumption Frequency', min_value=0.0, max_value=3.0, value=2.0)
+    NCP = st.slider('Number of Meals per Day', min_value=0.0, max_value=3.0, value=2.0)
+    CAEC = st.selectbox('Consumption of Food Between Meals', ('no', 'Sometimes', 'Frequently', 'Always')) 
+    SMOKE = st.selectbox('Smoking Habit', ('yes', 'no'))
+    CH2O = st.slider('Daily Water Intake', min_value=0.0, max_value=3.0, value=2.0)
+    SCC = st.selectbox('Caloric Drinks Consumption', ('yes', 'no'))
+    FAF = st.slider('Physical Activity Frequency', min_value=0.0, max_value=3.0, value=2.0)
+    TUE = st.slider('Time Using Technology', min_value=0.0, max_value=3.0, value=2.0)
+    CALC = st.selectbox('Alcohol Consumption', ('no', 'Sometimes', 'Frequently', 'Always')) 
+    MTRANS = st.selectbox('Transportation Mode', ('Automobile', 'Bike', 'Motorbike', 'Public_Transportation', 'Walking'))
+
+    user_input = [gender, age, height, weight, family_history_with_overweight, 
+                  FAVC, FCVC, NCP, CAEC, SMOKE, CH2O, SCC, FAF, TUE, CALC, MTRANS]
     
-  prediction_proba = model.predict_proba(df_input)
-  df_prediction_proba = pd.DataFrame(prediction_proba, columns=[
-      'Insufficient Weight', 'Normal Weight', 'Overweight Level I', 
-      'Overweight Level II', 'Obesity Type I', 'Obesity Type II', 'Obesity Type III'
-  ])
-    
-  st.write('Obesity Prediction Probability')
-  st.write(df_prediction_proba)
-  st.write('The predicted output is:', prediction)
+    df_input = input_to_df(user_input)
+
+    st.write('Data input by user')
+    st.write(df_input)
+
+    df_input = encode(df_input)
+    df_input = normalize(df_input)
+
+    prediction = predict_with_model(model, df_input)
+
+    prediction_proba = model.predict_proba(df_input)
+    df_prediction_proba = pd.DataFrame(prediction_proba, columns=[
+        'Insufficient Weight', 'Normal Weight', 'Overweight Level I', 
+        'Overweight Level II', 'Obesity Type I', 'Obesity Type II', 'Obesity Type III'
+    ])
+
+    st.write('Obesity Prediction Probability')
+    st.write(df_prediction_proba)
+    st.write('The predicted output is:', prediction)
 
 if __name__ == "__main__":
-  main()
+    main()
